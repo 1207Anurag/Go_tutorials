68c1d6c6e2ae160c9c9dfe38e47a7d69c72f2c53
Anurag Chaubey <anurag.chaubey@zopsmart.com>
2022-02-10 12:06:25 +0530

    adding a reducer file and reducer notes

---
+++ Reducer.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	numbers := []int{1, 2, 3, 4}
+	result := reduce(numbers, add)
+	fmt.Println(result)
+	result1 := reduce(numbers, sub)
+	fmt.Println(result1)
+}
+
+func reduce(nums []int, method func(int, int) int) int {
+	//these all are just function signature for add
+	//and then we are calling in the method
+	result := 0
+	for _, element := range nums {
+		result = method(result, element)
+		//calling the method for every element, in the slice
+	}
+	return result
+}
+func add(a, b int) int {
+	return a + b
+}
+func sub(a, b int) int {
+	return a - b
+}
